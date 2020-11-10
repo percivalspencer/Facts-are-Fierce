3b34cd325a2ab553f7f854bb9792b7286ba644ff
Spencer Percival <56115651+percivalspencer@users.noreply.github.com>
2020-11-10 23:40:18 +0000

    Create code block for first tab

--- server.R
+++ server.R
@@ -1,6 +1,11 @@
 # Define server logic required for app
 function(input, output, session) {
 
-    isolate({updateNavbarPage(session, inputId = "mainNav", selected = "tab1")})
+  isolate({updateNavbarPage(session, inputId = "mainNav", selected = "tab1")})
+
+  # Code for first tab (Meet the Queens)
+  observeEvent(c(input$seasonTab1, input$refreshTab1), {
+
+  })
 
 }
