156ccf477be30eb969c030136226bd477df8dd2a
Spencer Percival <56115651+percivalspencer@users.noreply.github.com>
2020-11-10 13:59:51 +0000

    Add global function to get data from API and return a data frame

---
+++ global.R
@@ -0,0 +1,19 @@
+getData <- function(infoRequested) {
+  # Load required libraries
+  library(httr)
+  library(jsonlite)
+
+  # API call
+  URL <- paste0("http://www.nokeynoshade.party/api/", infoRequested)
+
+  # Send http request
+  response <- httr::GET(URL)
+
+  # Read / unpack a binary file
+  dataJSON <- readBin(response$content, "text")
+
+  # Parse JSON file
+  data <- jsonlite::fromJSON(dataJSON)
+
+  return(data)
+}
