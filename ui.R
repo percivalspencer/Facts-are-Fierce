bfeab6bb26d739def878492cdd0b95785c842f74
Spencer Percival <56115651+percivalspencer@users.noreply.github.com>
2020-11-11 15:15:05 +0000

    Add comment explaining why CSS file is included

--- ui.R
+++ ui.R
@@ -2,30 +2,30 @@ library(shinythemes)
 
 # Define UI for application
 navbarPage(title = "Facts are Fierce", id = "mainNav", theme = shinytheme("cyborg"),
-
+           # Include custom CSS formatting
            includeCSS("lewk.css"),
 
            tabPanel("Meet the Queens", value = "tab1",
                     sidebarLayout(
-                        sidebarPanel(
-                            selectInput(inputId = "seasonTab1",
-                                           label = "Season of Drag Race",
-                                           choices = seasonNumbers,
-                                           multiple = FALSE,
-                                           selectize = TRUE,
-                                           width = NULL,
-                                           size = NULL),
-                            actionButton(inputId = "refreshTab1",
-                                         label = "Refresh",
-                                         icon = icon("refresh"),
-                                         width = NULL)
-                        ),
+                      sidebarPanel(
+                        selectInput(inputId = "seasonTab1",
+                                    label = "Season of Drag Race",
+                                    choices = seasonNumbers,
+                                    multiple = FALSE,
+                                    selectize = TRUE,
+                                    width = NULL,
+                                    size = NULL),
+                        actionButton(inputId = "refreshTab1",
+                                     label = "Refresh",
+                                     icon = icon("refresh"),
+                                     width = NULL)
+                      ),
 
-                        mainPanel(
-                          fluidRow(
-                            uiOutput("images")
-                          )
+                      mainPanel(
+                        fluidRow(
+                          uiOutput("images")
                         )
+                      )
                     )
            )
 )
